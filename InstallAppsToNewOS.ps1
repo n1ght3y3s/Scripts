024269534e147532a01de9a2a843792ea22c7cd0
Michael Eldridge <Michael.Eldridge@medmont.com>
2021-10-12 19:07:03 +1100

    Added initial version of powershell script for installing base apps in choco.

---
+++ InstallAppsToNewOS.ps1
@@ -0,0 +1,21 @@
+# Need to run the following first before running this script.
+# Set-ExecutionPolicy RemoteSigned
+
+Write-Output Installing apps
+
+Write-Output Configure chocolatey
+choco feature enable -n allowGlobalConfirmation
+
+choco install git
+choco install vscode
+choco install brave 
+choco install 7zip 
+choco install nodejs
+choco install windirstat 
+choco install tor-browser 
+choco install putty 
+choco install ilspy
+choco install firefox
+choco install gitkraken
+choco install gimp
+choco install obsidian
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Scripts
 A collection of various scripts I have found helpful over time.
+
+## Install Apps to New OS (PowerShell)
+Installs a set of useful applications on a fresh OS via the [Chocolately](https://chocolatey.org/) package/install manager. Specific for Windows OS.
