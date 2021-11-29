b23f3bb0ffcc84661c1c71c73f0878bae9d1311b
Michael Eldridge <michael.eldridge@medmont.com>
2021-11-29 14:36:54 +1100

    Replaced the echo command with the delete command.

--- DeleteOldFiles.cmd
+++ DeleteOldFiles.cmd
@@ -6,7 +6,7 @@ CALL :CleanUp C:\Temp
 EXIT /B %ERRORLEVEL%
 
 :CleanUp
-forfiles -p %1 -s -d -45 -c "cmd /c echo @path"
+forfiles -p %1 -s -d -45 -c "cmd /c del /s /q @path"
 
 rem Use a trick with robocopy. Using the same source and dest wont do anything
 rem but the /S /MOVE will clean up any folder that is empty.
